241b87e00c320ba3dc14edf36349de9f8816a6ae
boldak <boldak.andrey@gmail.com>
2024-11-25 18:44:13 +0200

    fix plugin init

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const router = require('express').Router()
+module.exports = {
+    init: async () => {
+        const router = require('express').Router()
 
-////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////
 
-const service = require("./src/service")
+        const service = require("./src/service")
 
-router.post("/get/", service.getStory)
-router.post("/release/", service.releaseStory)
-router.post("/update/", service.updateStory)
-router.post("/update-url/", service.updateUrl)
+        router.post("/get/", service.getStory)
+        router.post("/release/", service.releaseStory)
+        router.post("/update/", service.updateStory)
+        router.post("/update-url/", service.updateUrl)
 
-router.post("/create/", service.createStory)
+        router.post("/create/", service.createStory)
 
-router.post("/grant/", service.getGrant)
-router.post("/config/", service.getConfig)
-router.post("/list/", service.getList)
-router.post("/count/", service.getCount)
-
-
-////////////////////////////////////////////////////////////////////////////
+        router.post("/grant/", service.getGrant)
+        router.post("/config/", service.getConfig)
+        router.post("/list/", service.getList)
+        router.post("/count/", service.getCount)
 
 
-module.exports = router
+        ////////////////////////////////////////////////////////////////////////////
 
+        return router
+    }
+}
